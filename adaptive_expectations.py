51213965fb298c8ccaa9733e64a8ccd2311c64f8
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2025-02-20 19:50:57 +0100

    feat: add adaptive expectations page

---
+++ adaptive_expectations.py
@@ -0,0 +1,27 @@
+import plotly.io as pio
+import plotly.graph_objs as go
+
+class AdaptiveExpectations:
+    def __init__(self, p0, pN, c, periods):
+        self.p0 = p0
+        self.pN = pN
+        self.c = c
+        self.periods = periods
+
+    def ad_exp(self):
+        prices = [self.p0]
+        for t in range(1, self.periods + 1):
+            pt = prices[-1] + self.c * (self.pN - prices[-1])
+            prices.append(pt)
+        print("Final prices:", prices)
+        return list(range(self.periods + 1)), prices
+
+    def draw_graph(self, time_steps, prices):
+        trace = go.Scatter(x=time_steps, y=prices, mode='lines+markers', name='Expected price')
+        layout = go.Layout(xaxis=dict(title='Periods'), yaxis=dict(title='Price'))
+        figure = go.Figure(data=[trace], layout=layout)
+
+        return figure
+
+    def generate_graph(self, fig):
+        return pio.to_json(fig)
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, render_template, jsonify, url_for, redirect
 from cobweb import *
+from adaptive_expectations import *
 
 app = Flask(__name__)
 
 models = {
     "cobweb": {"name": "Cobweb Model", "fields": ["Demand shift", "Demand slope", "Supply shift", "Supply slope", "Iterations", "Initial price"]},
-    "demand": {"name": "Demand Model", "fields": ["Ціна", "Обсяг", "Еластичність"]},
-    "supply": {"name": "Supply Model", "fields": ["Кількість", "Витрати", "Рентабельність"]},
+    "adapt_exp": {"name": "Adaptive expectations", "fields": ["Previous price", "Normal price", "Adjustment factor", "Periods"]},
 }
 
 cobweb = 0
+adapt_exp = 0
 
 @app.route("/")
 def home():
@@ -17,27 +18,44 @@ def home():
 
 @app.route("/model", methods=["GET"])
 def model_page():
-    return render_template("data.html", model=get_model_name(), graph_json=cobweb)
+    model_name = request.args.get('name')
+    if not model_name:
+        return jsonify({"error": "Missing model name"}), 400
+
+    model = models.get(model_name)
+
+    if not model:
+        return jsonify({"error": "Unknown model"}), 400
+
+    graph_json = cobweb if model_name == "cobweb" else adapt_exp
+
+    return render_template("data.html", model=model, graph_json=graph_json)
+
 
 @app.route("/model", methods=["POST"])
 def model_page_post():
+    model_name = request.args.get('name')
+    print("model name:", model_name)
+
     data = request.json
 
     if not data:
-        return "Помилка: не отримано JSON", 400
+        return "Error: JSON is not found", 400
     else:
-        print("Отримані дані:", data)
+        print("Data obtained:", data)
 
-    cobweb = Cobweb(data["demand shift"], data["demand slope"], data["supply shift"], data["supply slope"], data["iterations"], data["initial price"])
+    if model_name == "cobweb":
+        cobweb = Cobweb(data["demand shift"], data["demand slope"], data["supply shift"], data["supply slope"], data["iterations"], data["initial price"])
+        return jsonify({"graph_json": cobweb.generate_graph(cobweb.find_cobweb())})
+    else:
+        adapt_exp = AdaptiveExpectations(data["previous price"], data["normal price"], data["adjustment factor"], data["periods"])
+        time_steps, prices = adapt_exp.ad_exp()
+        figure = adapt_exp.draw_graph(time_steps, prices)
+        return jsonify({"graph_json": adapt_exp.generate_graph(figure)})
 
-    return jsonify({"graph_json": cobweb.generate_graph(cobweb.find_cobweb())})
-
-def get_model_name():
-    model_name = request.args.get("name")
-    model = models.get(model_name)
-    if not model:
-        return "Model is not found", 404
-    return model
+@app.route("/calculator", methods=["GET"])
+def calc_page():
+    return render_template("calculator.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
