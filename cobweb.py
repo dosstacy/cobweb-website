c7a278226113bcd6e3fa54b2701f567b21dd97b2
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2025-03-03 21:45:13 +0100

    refactor: change cobweb graph logic

--- cobweb.py
+++ cobweb.py
@@ -1,8 +1,9 @@
-import plotly.graph_objs as go
-import plotly.io as pio
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
 
-class Cobweb:
+
+class EqCobweb:
     def __init__(self, d_shift, d_slope, s_shift, s_slope, n_iterations, initial_price):
         self.d_shift = d_shift
         self.d_slope = d_slope
@@ -11,26 +12,15 @@ class Cobweb:
         self.n_iterations = n_iterations
         self.initial_price = initial_price
 
-# d_shift = 10
-# d_slope = 1
-# s_shift = 2
-# s_slope = 0.8
-# n_iterations = 15
-# initial_price = 5
-
     def demand(self, price):
         return self.d_shift - self.d_slope * price
 
     def supply(self, price):
         return self.s_shift + self.s_slope * price
 
-
     def find_cobweb(self):
-        # Розрахунок рівноваги
-        equilibrium_price = (self.d_shift - self.s_shift) / (self.d_slope + self.s_slope)
-        equilibrium_quantity = self.demand(equilibrium_price)
+        pe = (self.d_shift - self.s_shift) / (self.s_slope + self.d_slope)
 
-        # Побудова павутини
         prices = [self.initial_price]
         quantities = [self.demand(self.initial_price)]
 
@@ -38,35 +28,42 @@ class Cobweb:
             if i % 2 == 0:
                 quantities.append(self.demand(prices[-1]))
             else:
-                prices.append((quantities[-1] - self.s_shift) / self.s_slope)
+                prices.append((self.initial_price - pe) * (self.s_slope / self.d_slope) ** i + pe)
 
         if len(prices) < len(quantities):
             prices.append((quantities[-1] - self.s_shift) / self.s_slope)
 
-        # Побудова графіка
         price_range = np.linspace(0, self.n_iterations, 500)
 
         fig = go.Figure()
 
-        # Лінії попиту та пропозиції
-        fig.add_trace(go.Scatter(x=price_range, y=self.demand(price_range), mode='lines', name="Demand", line=dict(color="blue")))
-        fig.add_trace(go.Scatter(x=price_range, y=self.supply(price_range), mode='lines', name="Supply", line=dict(color="red")))
+        fig.add_trace(
+            go.Scatter(x=price_range, y=self.demand(price_range), mode='lines', name="Demand", line=dict(color="blue")))
+        fig.add_trace(
+            go.Scatter(x=price_range, y=self.supply(price_range), mode='lines', name="Supply", line=dict(color="red")))
 
-        # Павутинні лінії
         for i in range(1, len(prices)):
-            fig.add_trace(go.Scatter(x=[prices[i-1], prices[i-1]], y=[quantities[i-1], quantities[i]], mode='lines', line=dict(color="black", dash="dash"),showlegend=False))
-            fig.add_trace(go.Scatter(x=[prices[i-1], prices[i]], y=[quantities[i], quantities[i]], mode='lines', line=dict(color="black", dash="dash"),showlegend=False))
+            fig.add_trace(
+                go.Scatter(x=[prices[i - 1], prices[i - 1]], y=[quantities[i - 1], quantities[i]], mode='lines',
+                           line=dict(color="black", dash="dash"), showlegend=False))
+            fig.add_trace(go.Scatter(x=[prices[i - 1], prices[i]], y=[quantities[i], quantities[i]], mode='lines',
+                                     line=dict(color="black", dash="dash"), showlegend=False))
 
-        # Точка рівноваги
-        fig.add_trace(go.Scatter(x=[equilibrium_price], y=[equilibrium_quantity], mode='markers', marker=dict(color="green", size=10), name="Equilibrium"))
+        fig.add_trace(go.Scatter(x=[pe], y=[pe], mode='markers',
+                                 marker=dict(color="green", size=10), name="Equilibrium"))
 
-        # Оформлення
         fig.update_layout(xaxis_title="Price", yaxis_title="Amount", template="plotly_white")
 
-        return fig
+        fig.show()
 
-    def generate_image(self, fig):
-        fig.write_image("cobweb.png")
 
-    def generate_graph(self, fig):
-        return pio.to_json(fig)
+# Приклад використання
+model = EqCobweb(
+    d_shift=20,  # зсув попиту
+    d_slope=2,  # нахил попиту
+    s_shift=10,  # зсув пропозиції
+    s_slope=1,  # нахил пропозиції
+    n_iterations=10,  # кількість ітерацій
+    initial_price=10  # початкова ціна
+)
+model.find_cobweb()
