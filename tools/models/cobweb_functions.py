8e105e269d7b0b2aefb048edc0a38c23c95adf6d
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2025-04-24 21:41:33 +0200

    make button from logo for main page

--- tools/models/cobweb_functions.py
+++ tools/models/cobweb_functions.py
@@ -3,9 +3,7 @@ from tools import convert_to_sympy
 
 ##TODO: 1. edit text pages
 ##TODO: 4. add modal window for solution error
-##TODO: 7. edit main page
 ##TODO: 9. придумати якийсь прикол з n-1
-##TODO: кнопка для напису models
 
 class FuncCobweb:
     def __init__(self, str_func, x_min, x_max, y_min, y_max, seed, iterates):
