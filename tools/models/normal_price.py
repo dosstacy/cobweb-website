3aed5ef935eaa4e0c8087e8c49b8dbb370866ebe
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2025-05-06 16:56:57 +0200

    change equation for normal price and adaptive expectation models

--- tools/models/normal_price.py
+++ tools/models/normal_price.py
@@ -1,17 +1,4 @@
 class NormalPrice:
-    # def __init__(self, p0, pN, c, periods):
-    #     self.p0 = p0
-    #     self.pN = pN
-    #     self.c = c
-    #     self.periods = periods
-    #
-    # # def ad_exp(self):
-    #     prices = [self.p0]
-    #     for t in range(1, self.periods + 1):
-    #         pt = prices[-1] + self.c * (self.pN - prices[-1])
-    #         prices.append(pt)
-    #     print("Final prices:", prices)
-    #     return list(range(self.periods + 1)), prices
 
     def __init__(self, demand_shift, demand_slope, supply_shift, supply_slope, factor, periods, constant):
         self.demand_shift = demand_shift
@@ -29,6 +16,9 @@ class NormalPrice:
         for t in range(1, self.periods):
             pt = self.constant * ((self.supply_slope * (1 - self.factor))/ self.demand_slope) ** t + (
                             (self.supply_shift - self.demand_shift) / (self.demand_slope - self.supply_slope))
+
+            print("Equilibrium price")
+            print((self.supply_shift - self.demand_shift) / (self.demand_slope - self.supply_slope))
             prices.append(pt)
             times.append(t)
 
